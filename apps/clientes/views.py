4c5cde5d487f97ac6a56a32454804e6d3e5c6937
Nathália Cericatto <nathalia.cericatto@gmail.com>
2026-05-19 19:50:18 -0300

    Implementa cliente view

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Cliente
+from .serializer import ClientesSerializer
 # Create your views here.
+class ClientesViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClientesSerializer  
