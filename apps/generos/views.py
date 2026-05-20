5e2e64d994d9162b9ff82900918ca0e1ecb99cd6
LuizFilipeAv <luizfilipelavila@gmail.com>
2026-05-20 19:54:45 -0300

    Implementa a view GeneroViewSet com queryset e serializer

--- apps/generos/views.py
+++ apps/generos/views.py
@@ -3,3 +3,6 @@ from .models import Genero
 from rest_framework import viewsets
 from .serializer import GeneroSerializer
 # Create your views here.
+class GeneroViewSet(viewsets.ModelViewSet):
+    queryset = Genero.objects.all()
+    serializer_class = GeneroSerializer
