f0c542ccc659cf875e7b4d987f967cc58107b469
LuizFilipeAv <luizfilipelavila@gmail.com>
2026-05-20 19:32:50 -0300

    Adiciona modelo Genero com campos nome, descricao e icone

--- apps/generos/models.py
+++ apps/generos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Genero(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    descricao = models.TextField('Descrição')
+    icone = models.CharField('Ícone', max_length=255)
+
+    class Meta:
+        verbose_name = 'Gênero'
+        verbose_name_plural = 'Gêneros'
+
+    def __str__(self):
+        return self.nome
