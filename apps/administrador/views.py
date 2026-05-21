1389bfc31c45f7cc136c3be9650b4bd50aa8f8b7
Nathália Cericatto <nathalia.cericatto@gmail.com>
2026-05-21 20:01:40 -0300

    Implementa view admin

--- apps/administrador/views.py
+++ apps/administrador/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render, redirect
 from .models import Administrador
-from .serializer import AdministradorSerializer
-# Create your views here.
-class AdministradorViewSet(viewsets.ModelViewSet):
-    queryset = Administrador.objects.all()
-    serializer_class = AdministradorSerializer  
+
+def cadastrar(request):
+    template_name = 'administrador/cadastrar.html'
+    
+    if request.method == 'POST':
+        Administrador.objects.create(
+            nome=request.POST.get('nome'),
+            cpf=request.POST.get('cpf'),
+            email=request.POST.get('email'),
+            senha=request.POST.get('senha'),
+            nivel_acesso=request.POST.get('nivel_acesso'),
+        )
+        return redirect('usuarios:login')
+    
+    return render(request, template_name)
