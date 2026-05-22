3595a662ec935b9e47af94c9ccce976f57d11b81
Miqueias Allisson <miqueias.allisson@gmail.com>
2026-05-22 16:09:34 -0300

    Cria views de Pedido

--- apps/pedido/views.py
+++ apps/pedido/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pedido
 
 # Create your views here.
+def listar_pedidos(request):
+    pedidos = Pedido.objects.all()
+    return render(request, 'pedido/listar.html', {'pedidos': pedidos})
+
+
+def detalhe_pedido(request, pedido_id):
+    pedido = Pedido.objects.get(id=pedido_id)
+    return HttpResponse(f'Pedido {pedido.id} - Status: {pedido.status}')
