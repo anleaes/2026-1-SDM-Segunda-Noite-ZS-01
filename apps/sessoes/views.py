fc120dd582ec7eceb063b42dbac4285d72ec0801
LuizFilipeAv <luizfilipelavila@gmail.com>
2026-05-22 20:10:14 -0300

    Adiciona a classe 'SessaoViewSet' para gerenciar as visualizações de sessões

--- apps/sessoes/views.py
+++ apps/sessoes/views.py
@@ -3,3 +3,6 @@ from .models import Sessao
 from rest_framework import viewsets
 from .serializer import SessaoSerializer
 # Create your views here.
+class SessaoViewSet(viewsets.ModelViewSet):
+    queryset = Sessao.objects.all()
+    serializer_class = SessaoSerializer
