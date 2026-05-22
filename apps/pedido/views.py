89a7ec7fcfa4dfc5f30d87371125fb20030202d4
Miqueias Allisson <miqueias.allisson@gmail.com>
2026-05-22 17:32:26 -0300

    feat: Atualiza views de Pedido para retornar dados em formato JSON

--- apps/pedido/views.py
+++ apps/pedido/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 from .models import Pedido
 
-# Create your views here.
+
 def listar_pedidos(request):
-    pedidos = Pedido.objects.all()
-    return render(request, 'pedido/listar.html', {'pedidos': pedidos})
+    pedidos = list(Pedido.objects.values('id', 'status', 'dataHora'))
+    return JsonResponse(pedidos, safe=False)
 
 
 def detalhe_pedido(request, pedido_id):
-    pedido = Pedido.objects.get(id=pedido_id)
     pedido = get_object_or_404(Pedido, id=pedido_id)
-    return HttpResponse(f'Pedido {pedido.id} - Status: {pedido.status}')
+    return JsonResponse({'id': pedido.id, 'status': pedido.status, 'dataHora': pedido.dataHora})
