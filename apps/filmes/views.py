f5c169e0692406700d6acd7c716daeb894d1beaf
LuizFilipeAv <luizfilipelavila@gmail.com>
2026-05-21 22:17:47 -0300

    Adiciona a classe FilmeViewSet para gerenciamento de visualizações de filmes

--- apps/filmes/views.py
+++ apps/filmes/views.py
@@ -3,3 +3,6 @@ from .models import Filme
 from rest_framework import viewsets
 from .serializer import FilmeSerializer
 # Create your views here.
+class FilmeViewSet(viewsets.ModelViewSet):
+    queryset = Filme.objects.all()
+    serializer_class = FilmeSerializer
