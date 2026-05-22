02b3b34c0075c2d6db11b15697ad7ebe1e28f94c
LuizFilipeAv <luizfilipelavila@gmail.com>
2026-05-21 22:22:00 -0300

    Adiciona configurações de rotas para o aplicativo de filmes

--- apps/filmes/urls.py
+++ apps/filmes/urls.py
@@ -2,3 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'filmes'
+router = routers.SimpleRouter()
+router.register('', views.FilmeViewSet, basename='filmes')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
