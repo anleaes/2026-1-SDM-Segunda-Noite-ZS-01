fd28765d628e9a3193289a5034a01410f18cf36f
LuizFilipeAv <luizfilipelavila@gmail.com>
2026-05-22 20:12:54 -0300

    Adiciona configuração de rotas para a aplicação de sessões

--- apps/sessoes/urls.py
+++ apps/sessoes/urls.py
@@ -2,3 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'sessoes'
+router = routers.SimpleRouter()
+router.register('', views.SessaoViewSet, basename='sessoes')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
