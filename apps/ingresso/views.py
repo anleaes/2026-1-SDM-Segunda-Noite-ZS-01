73198470b4fb13336dd13aebfccfcc8b09ceb277
Miqueias Allisson <miqueias.allisson@gmail.com>
2026-05-24 16:53:42 -0300

    feat: criando views ingresso

--- apps/ingresso/views.py
+++ apps/ingresso/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework import viewsets
+
+from .models import Ingresso
+from .serializer import IngressoSerializer
 
 # Create your views here.
