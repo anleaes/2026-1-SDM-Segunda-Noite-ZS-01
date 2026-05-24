7895649e6b624200fe3fdd67ef94770cfeb1f94b
Miqueias Allisson <miqueias.allisson@gmail.com>
2026-05-24 16:27:17 -0300

    feat: adicionando o PagamentoView

--- apps/pagamento/views.py
+++ apps/pagamento/views.py
@@ -3,3 +3,6 @@ from .models import Pagamento
 from .serializers import PagamentoSerializer
 
 # Create your views here.
+class PagamentoViewSet(viewsets.ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer
