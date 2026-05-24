c7f7ef9223e5f5e5e203b648c2ff1f4d64479a8b
Miqueias Allisson <miqueias.allisson@gmail.com>
2026-05-24 16:50:51 -0300

    Cria model Ingresso

--- apps/ingresso/models.py
+++ apps/ingresso/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from pedido.models import Pedido
 
 # Create your models here.
+class Ingresso(models.Model):
+    STATUS_CHOICES = [
+        ('criado', 'Criado'),
+        ('validado', 'Validado'),
+        ('cancelado', 'Cancelado'),
+    ]
+
+    pedido = models.ForeignKey(
+        Pedido,
+        on_delete=models.CASCADE,
+        related_name='ingressos',
+        verbose_name='Pedido'
+    )
+
+    idSessao = models.IntegerField(
+        verbose_name='ID Sessão'
+    )
+
+    idAssento = models.IntegerField(
+        verbose_name='ID Assento'
+    )
+
+    codigoPR = models.CharField(
+        max_length=100,
+        verbose_name='Código PR'
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='criado',
+        verbose_name='Status'
+    )
+
+    ingressoMeiaEntrada = models.BooleanField(
+        default=False,
+        verbose_name='Ingresso Meia Entrada'
+    )
+
+    def validar(self):
+        self.status = 'validado'
+        self.save()
+        return True
+
+    def __str__(self):
+        return f'Ingresso {self.id} - Pedido {self.pedido_id} - {self.status}'
+
+    class Meta:
+        verbose_name = 'Ingresso'
+        verbose_name_plural = 'Ingressos'
