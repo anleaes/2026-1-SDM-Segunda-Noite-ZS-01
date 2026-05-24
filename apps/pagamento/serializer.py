09755c499acbe047337edf9205800523858222b6
Miqueias Allisson <miqueias.allisson@gmail.com>
2026-05-24 16:25:33 -0300

    feat: adicionando o  PagamentoSerializer

--- apps/pagamento/serializer.py
+++ apps/pagamento/serializer.py
@@ -2,3 +2,7 @@ from rest_framework import serializers
 from .models import Pagamento
 
 
+class PagamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pagamento
+        fields = '__all__'
