df0911a94fcaeda72b85cc725f8ed4be06720bc6
Nathália Cericatto <nathalia.cericatto@gmail.com>
2026-05-28 21:17:04 -0300

    Cria view home

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    template_name = 'core/base.html'
-    context = {}
+    usuario_tipo = request.session.get('usuario_tipo', 'visitante')
+    template_name = 'core/home.html'
+    context = {
+        'usuario_tipo': usuario_tipo,
+        'usuario_nome': request.session.get('usuario_nome'),
+        'token': request.session.get('token'),
+    }
     return render(request, template_name, context)
